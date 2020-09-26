7838788cd9edbdc2376b2fec015f397aac2fd698
tungdeptraib5 <tungtrinh99@gmail.com>
2020-09-26 22:55:58 +0700

    make it better

--- src/app.js
+++ src/app.js
@@ -20,6 +20,6 @@ app.set('views', path.join(__dirname, 'resources','views'));
 
 routers(app) 
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`App listening at http://localhost:${port}`)
   })
